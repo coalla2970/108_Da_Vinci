aa592d90c2d1cc6c56c7fd2aeabedf2f56320e3d
ksho719 <62803595+ksho719@users.noreply.github.com>
2020-04-29 23:51:44 +0900

    Added description and removed checkpoints

--- recall_data.cpp
+++ recall_data.cpp
@@ -11,7 +11,7 @@
 #include "recall_data.h"
 #include "card.h"
 using namespace std;
-
+//recall_data function recalls saved data from external file and update users' information to the current variable
 void recall_data(card cards1[], card cards2[], card deck[],int &numbercards1, int &numbercards2,
 int &password1, int &password2, string &player1, string &player2, int joker1[], int joker2[], int &turn, string &recall){
   int linecount = 1;
@@ -31,6 +31,7 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
         cout<< "Error in opening file!" <<endl;
         exit(1);
       }
+      //recalling player1's information
       while(getline(fin,player1line)){
         stringstream player1data(player1line);
         if (linecount==1){
@@ -38,21 +39,18 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
             player1 = tempstr;
           }
           linecount++;
-          cout << "check1";
         }
         else if (linecount==2){
           while (player1data >> tempno){
             password1 = stoi(tempno,nullptr,10);
           }
           linecount++;
-          cout << "check2";
         }
         else if (linecount==3){
           while (player1data >> tempno){
             numbercards1 = stoi(tempno,nullptr,10);
           }
           linecount++;
-          cout << "check2";
         }
         else if (linecount==4){
           while (player1data >> tempchar){
@@ -61,7 +59,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check3";
         }
         else if (linecount==5){
           while (player1data >> tempno){
@@ -70,7 +67,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check4";
         }
         else if (linecount==6){
           while (player1data >> tempno){
@@ -79,7 +75,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check5";
         }
         else{
           linecount = 1;
@@ -93,6 +88,7 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
         cout<< "Error in opening file!" <<endl;
         exit(1);
       }
+      //recalling player2's information
       while(getline(fin,player2line)){
         stringstream player2data(player2line);
         if (linecount==1){
@@ -100,21 +96,18 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
             player2 = tempstr;
           }
           linecount++;
-          cout << "check1";
         }
         else if (linecount==2){
           while (player2data >> tempno){
             password2 = stoi(tempno,nullptr,10);
           }
           linecount++;
-          cout << "check2";
         }
         else if (linecount==3){
           while (player2data >> tempno){
             numbercards2 = stoi(tempno,nullptr,10);
           }
           linecount++;
-          cout << "check2";
         }
         else if (linecount==4){
           while (player2data >> tempchar){
@@ -123,7 +116,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check3";
         }
         else if (linecount==5){
           while (player2data >> tempno){
@@ -132,7 +124,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check4";
         }
         else if (linecount==6){
           while (player2data >> tempno){
@@ -141,7 +132,6 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
           }
           idx=0;
           linecount++;
-          cout << "check5";
         }
         else{
           linecount = 1;
@@ -155,6 +145,7 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
         cout<< "Error in opening file!" <<endl;
         exit(1);
       }
+      //recalling cards deck's information
       while(getline(fin,deckline)){
         stringstream deckdata(deckline);
         if (linecount==1){
@@ -211,11 +202,10 @@ int &password1, int &password2, string &player1, string &player2, int joker1[], 
       break;
     }
     else if(recall=="N"){
-      cout << "Starting new game!" << endl;
       break;
     }
     else{
-      cout << "Please put valid input"<<endl;
+      cout << "Please put a valid input"<<endl;
       cout << "Do you want to recall previous data? (Y/N):";
       cin >> recall;
       continue;
